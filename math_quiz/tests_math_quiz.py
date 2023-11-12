d0bcec903bf720158d1014d0d51280b78ca89778
Ali <nasiry.ali77@gmail.com>
2023-11-12 18:52:05 +0100

    Modify tests_math_quiz

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import gen_rand_int, gen_rand_operator, sub_mul_add
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,22 +9,29 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = gen_rand_int(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        operator = ['+', '-', '*']
+        for _ in range(1000):
+             rand_op = gen_rand_operator()
+             self.assertIn(rand_op, operator)
+             self.assertIsInstance(rand_op, str)
 
+        
     def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (8, 3, '-', '8 - 3', 5),
+            (4, 6, '*', '4 * 6', 24),
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = sub_mul_add(num1, num2, operator)
+            self.assertEqual(problem, expected_problem)
+            self.assertEqual(answer, expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
